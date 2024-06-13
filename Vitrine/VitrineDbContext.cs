034c4d5ce54199a76923075b667756264c295365
Thiago Silverio <housezx210@gmail.com>
2024-06-13 00:23:01 -0300

    Primeiro commit

---
+++ Vitrine/VitrineDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+using Vitrine.Model;
+
+namespace Database;
+
+public partial class VitrineDbContext : DbContext
+{
+    public DbSet<Produto> Produtos { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
+
+    public VitrineDbContext()
+    {
+    }
+
+    public VitrineDbContext(DbContextOptions<VitrineDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=localhost;userid=root;database=vitrine", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.4.21-mariadb"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_general_ci")
+            .HasCharSet("utf8mb4");
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
